7fec4aec4e4b67b157abc6143c1830a0e2b5fb69
Mop321 <aweds200@gmail.com>
2025-09-08 16:05:05 +0300

    as1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateReceipt from "./pages/createRec";
 import Receipt from "./pages/receipt";
