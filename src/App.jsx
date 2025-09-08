6be0e9f4f65189cddf3c9fa4bf4866b91991f981
Mop321 <aweds200@gmail.com>
2025-09-08 15:45:41 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateReceipt from "./pages/createRec";
 import Receipt from "./pages/receipt";
 import Home from "./pages/Home";
+
+const url = "https://backup-1-4pfb.onrender.com";
 function App() {
   const [receiptData, setReceiptData] = useState(null);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const res = await fetch(`${url}/users`);
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.error("API error:", err);
+      }
+    }
+    fetchUsers();
+  }, []);
 
   const handleCreate = (data) => {
     setReceiptData(data);
@@ -13,7 +28,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home></Home>} />
+        <Route path="/" element={<Home />} />
         <Route
           path="/create"
           element={<CreateReceipt addReceipt={handleCreate} />}
